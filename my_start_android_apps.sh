5bb504c15aaa1282cfe06c060802d27c93110ff0
kachnu <ya.kachnu@yandex.ua>
2016-01-13 15:58:56 +0000

    20160113

--- my_start_android_apps.sh
+++ my_start_android_apps.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 #Сделано на основании статьи http://vasilisc.com/android-app-in-linux
-#Скрипт для запуска android-приложений *.apk а также подготовленных для запуска файлов ARCon
+#Скрипт для запуска android-приложений *.apk, а также подготовленных для запуска файлов ARCon
 #author: kachnu
 # email: ya.kachnu@yandex.ua
 
+WORK_FOLDER="$HOME/.start_android_apps"
+
+if [ ! -d  "$WORK_FOLDER" ]
+ then mkdir -p "$WORK_FOLDER"
+fi
+
+if [ ! -f  "$WORK_FOLDER/start_android_apps.conf" ]
+ then echo '
 #ссылка на скачивание ARCon https://github.com/vladikoff/chromeos-apk/blob/master/archon.md
 WGET_ARCON="http://archon.vf.io/ARChon-v1.2-x86_32.zip"
 
@@ -14,18 +22,28 @@ WGET_NODE="https://nodejs.org/download/release/latest-v5.x/node-v5.4.0-linux-x86
 WGET_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb"
 
 #указывается путь к папке с ARCon
-FOLDER_WITH_ARCHON="$HOME/tmp/vladikoff-archon-2d4c947b3f04"
+FOLDER_WITH_ARCHON="$WORK_FOLDER/vladikoff-archon-2d4c947b3f04"
 
 #указывается путь к бинарнику node
-FOLDER_WITH_NODE="$HOME/tmp/node-v5.4.0-linux-x86/bin"
+FOLDER_WITH_NODE="$WORK_FOLDER/node-v5.4.0-linux-x86/bin"
 
-TEMP_FOLDER="$HOME/tmp/"
-MY_TERMINAL="x-terminal-emulator" #Присваивание терминала 
-DIALOG=zenity #Присваивание типа графического диалогового окна
+#Присваивание терминала
+MY_TERMINAL="x-terminal-emulator" 
+
+#Присваивание типа графического диалогового окна
+DIALOG=zenity
+
+#Присваивание типа текстового редактора
+TEXT_EDITOR=geany
+' > "$WORK_FOLDER/start_android_apps.conf"
+fi
+
+source "$WORK_FOLDER/start_android_apps.conf"
+
 FILE_APK="$1"
 
 HELP="НАИМЕНОВАНИЕ
-     Скрипт $0 позволяет запустить файлы *.ark и предварительно подготовленные ARChon файлы (указываются папки) 
+     Скрипт $0 позволяет запустить файлы *.apk и предварительно подготовленные ARChon файлы (указываются папки) 
 СИНТАКСИС
 	$0 [КЛЮЧ] [Файл или Папка]
 КЛЮЧИ
@@ -33,13 +51,13 @@ HELP="НАИМЕНОВАНИЕ
 	--install - устанавливает необходимые пакеты и программы для работы с *.apk
 	--gui - запуск скрипта в графическом диалоговом окне	
 ПРИМЕРЫ ИСПОЛЬЗОВАНИЯ СКРИПТА
-	$0 \"/home/user/Game.apk\" - файл *.ark или предварительно подготовленную ARChon папку
+	$0 \"/home/user/Game.apk\" - файл *.apk или предварительно подготовленную ARChon папку
 	$0 --gui - откроет диалоговое окно поиска
 	$0 -h - выводит справку"
 ##########################################
 InstallPackages ()
 {
-cd $TEMP_FOLDER
+cd "$WORK_FOLDER"
 if [ "$ERROR_MASSAGE" != '' ]
  then
    for opt in $ERROR_MASSAGE
@@ -72,8 +90,7 @@ if [ "$ERROR_MASSAGE" != '' ]
                            fi
                            ;;
      esac
-    done 
-    read x                      
+    done                    
  else echo "Все установлено, можно работать!"
 fi
 }
@@ -82,26 +99,26 @@ StartApk ()
 {
 if [ -d "$FILE_APK" ] #обрабатываем папку
  then FOLDER_APK="$FILE_APK"
-      google-chrome --enable-easy-off-store-extension-install --load-extension="$FOLDER_WITH_ARCHON" --load-and-launch-app="$FOLDER_APK" 
+      google-chrome --profile-directory=Default --enable-easy-off-store-extension-install --load-extension="$FOLDER_WITH_ARCHON" --load-and-launch-app="$FOLDER_APK" 
       #$DIALOG --info --title="$ATTENTION" --text="Пожалуйста, закройте google-chrome и повторите попытку"
 fi
 
 if [ -f "$FILE_APK" ] #обрабатываем файл *.apk
  then 
       if [ -w "$(dirname "$FILE_APK")" ]
-       then TEMP_FOLDER=$(dirname "$FILE_APK")
+       then WORK_FOLDER=$(dirname "$FILE_APK")
       fi   
-      cd $TEMP_FOLDER
+      cd $WORK_FOLDER
       export PATH=$PATH:$FOLDER_WITH_NODE
       chromeos-apk "$FILE_APK"
       NAME_FOLDER_APK=$(chromeos-apk "$FILE_APK" | awk '{print $3}')
       echo NAME_FOLDER_APK=$NAME_FOLDER_APK
-      FOLDER_APK="$TEMP_FOLDER/$NAME_FOLDER_APK"
+      FOLDER_APK="$WORK_FOLDER/$NAME_FOLDER_APK"
       echo FOLDER_APK=$FOLDER_APK
       if [ -d "$FOLDER_APK" ]
        then TEXT_TO_INCLUDE=$(cat "$FOLDER_APK/manifest.json" | grep -m1 name | sed "s/name/message/" | sed "s/,//")
             sed -i "s/\"description\": \"Extension name\"/\"description\": \"Extension name\", ${TEXT_TO_INCLUDE}/g" $FOLDER_APK/_locales/en/messages.json
-            google-chrome --enable-easy-off-store-extension-install --load-extension="$FOLDER_WITH_ARCHON" --load-and-launch-app="$FOLDER_APK" #&& rm -R "$FOLDER_APK"
+            google-chrome --profile-directory=Default --enable-easy-off-store-extension-install --load-extension="$FOLDER_WITH_ARCHON" --load-and-launch-app="$FOLDER_APK" #&& rm -R "$FOLDER_APK"
             #$DIALOG --info --title="$ATTENTION" --text="Пожалуйста, закройте google-chrome и повторите попытку"
       fi
 fi
@@ -137,8 +154,8 @@ if [ "$ERROR_MASSAGE" != '' ]
 Хотите скачать необходимые файлы и установить недостоющие пакеты?" 
       if [ $? == 0 ]
         then $MY_TERMINAL -e $0 --install
+        else exit 0
       fi
-      exit 1    
 fi
 }
 ##########################################
@@ -146,13 +163,14 @@ GuiForm ()
 {
 FILE_APK=`$DIALOG --file-selection --title="Выбирите файл *.apk "`
 if [ $? == 0 ]
- then $0 "$FILE_APK"
+ then StartApk
 fi     
 }
 ##########################################
 case "$FILE_APK" in
-            '') echo "Не указан аргумент. Чтобы получить дополнительную информацию, наберите: $0 -h "
-                exit 1
+            '') CheckPO
+                MessageError
+                GuiForm
                 ;;
      -h|--help) echo "$HELP"
                 read x
@@ -160,11 +178,7 @@ case "$FILE_APK" in
                 ;;
      --install) CheckPO
                 InstallPackages
-                ;;
-         --gui) CheckPO
-                MessageError
-                GuiForm
-                ;;     
+                ;;   
              *) CheckPO
                 MessageError
                 StartApk
