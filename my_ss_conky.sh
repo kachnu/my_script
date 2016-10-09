68680482222a932fe37cd889ca62cae45e43f001
kachnu <ya.kachnu@yandex.ua>
2016-10-09 13:05:09 +0000

    55555

--- my_ss_conky.sh
+++ my_ss_conky.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-#start conky in conky-manager
-
-#if pidof conky > /dev/null
- #then killall conky
- #else sh ~/.conky/conky-startup.sh
-#fi
-
-#kill $(ps -ela | grep $UID | grep conky | awk '{print $4}')
+# start conky in conky-manager
 
 sh ~/.conky/conky-startup.sh
 
