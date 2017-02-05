6e187627ca0144d0015a49faa2b05a60f6fcb980
kachnu <ya.kachnu@yandex.ua>
2017-02-05 15:51:46 +0200

    lkflxx

--- moc2panel.sh
+++ moc2panel.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
 
-# цвета шрифтов
-# указывается название цвета - green, red и т.д. или hex-код цвета - #0000FF
-FONT_COLOR_ARTIST="red"
-FONT_COLOR_SONG="#008000"
-FONT_COLOR_TIME="#0000FF"
-
-# цвет фона
-BG_COLOR="#E8E8E7"
-
-# Тип шрифта можно настроить в апплете xfce4-genmon-plugin
-# а можно добавить к echo параметр weight=\"$WEIGTH\"
-# при этом раскоментировать переменную WEIGTH и дать ей значения: regular - нормальный, bold - полужирный
-# WEIGTH="bold"
-
-function tagi
+# функция отображения тегов
+tagi ()
 {
-#title=`mocp -i |grep -w Title |cut -d : -f2`
-#songtitle=`mocp -i |grep -w SongTitle |cut -d : -f2`
-#artist=`mocp -i |grep -w Artist |cut -d : -f2`
-#rate=`mocp -i |grep -w Rate |cut -d : -f2`
-#bitrate=`mocp -i |grep -w Bitrate |cut -d : -f2`
-#timeleft=`mocp -i |grep -w TimeLeft |cut -d : -f2-`
-#currenttime=`mocp -i |grep -w CurrentTime |cut -d : -f2-`
+# разделитель при выводе информации
+separ=" -"
 
+# перебираем вывод команды mocp -i и раскладываем все по переменным
 IFS="
 "
-for line in $(mocp -i)
+for line in `mocp -i`
  do
     case $line in
+      State*) state=`echo $line|cut -d : -f2`
+              case $line in
+                   *PLAY*) state='▶';;
+                   *PAUSE*) state='❚❚';;
+                   *STOP*) state='■ moc'
+                           separ='';;
+              esac  ;;
       File*) file=`echo $line|cut -d : -f2`;;
       Title*) title=`echo $line|cut -d : -f2`;;
       Artist*) artist=`echo $line|cut -d : -f2`;;
@@ -42,20 +32,41 @@ for line in $(mocp -i)
     esac
 done
 
-echo "<txt>\
-<span bgcolor=\"$BG_COLOR\" foreground=\"$FONT_COLOR_ARTIST\">"$artist"</span>""\
-<span bgcolor=\"$BG_COLOR\" foreground=\"$FONT_COLOR_SONG\">"$songtitle"</span>""\
-<span bgcolor=\"$BG_COLOR\" foreground=\"$FONT_COLOR_TIME\">"$timeleft"</span>""\
-</txt>"
 
-#echo "<txt><span bgcolor=\"#333333\" weight=\"bold\" fgcolor=\"green\">"$artist" "" "$songtitle" "" "$timeleft"</span></txt><tool>mocp playing</tool>"
-#echo "<txt><span bgcolor=\"#1C2948\" fgcolor=\"#bebebe\">"$artist""$songtitle""$timeleft"</span></txt>"
+# выводим тег
+echo "$state\
+$artist\
+$separ\
+$songtitle\
+$separ\
+$timeleft"
+
+
+## цвета шрифтов
+## указывается название цвета - green, red и т.д. или hex-код цвета - #0000FF
+#FONT_COLOR_ARTIST="red"
+#FONT_COLOR_SONG="#008000"
+#FONT_COLOR_TIME="#0000FF"
+
+## цвет фона
+#BG_COLOR="#E8E8E7"
+
+## Тип шрифта можно настроить в апплете xfce4-genmon-plugin
+## а можно добавить к echo параметр weight=\"$WEIGTH\"
+## при этом раскоментировать переменную WEIGTH и дать ей значения: regular - нормальный, bold - полужирный
+## WEIGTH="bold"
+
+#echo "<txt>\
+#<span bgcolor=\"$BG_COLOR\" foreground=\"$FONT_COLOR_ARTIST\">"$artist"</span>""\
+#<span bgcolor=\"$BG_COLOR\" foreground=\"$FONT_COLOR_SONG\">"$songtitle"</span>""\
+#<span bgcolor=\"$BG_COLOR\" foreground=\"$FONT_COLOR_TIME\">"$timeleft"</span>""\
+#</txt>"
 }
 
-state=`mocp -i |grep -w State |cut -d : -f2`
-if [ $state=PLAY ]
- then
-  tagi
+# проверяем запущен ли mocp и если запущен выполняем функцию вывода тегов
+if [[ `pidof mocp` ]]
+  then tagi
+  else echo ""
 fi
 
 exit 0
