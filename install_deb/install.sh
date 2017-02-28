fc0043208677e6e7bb115a979256d1e047fd1ffd
kachnu <ya.kachnu@yandex.ua>
2017-02-28 09:07:50 +0000

    fldskf

---
+++ install_deb/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+error_apt ()
+{
+echo -e "
+
+The list of packages is not updated.
+Packages are not installed.
+Check your network connection and try again.
+
+Press Enter to exit"
+read x
+
+exit 1
+}
+sudo apt-get update || error_apt
+sudo dpkg -i *.deb
+sudo apt-get -y install -f
+
+echo "Press Enter to exit"
+read x
+
+exit 0
---
+++ install_deb/uninstall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DEB_LIST=`ls *.deb`
+UNINSTALL_LIST=""
+
+for DEB in $DEB_LIST; do
+  PACKAGE=`dpkg -I $DEB | grep -m1 Package: | awk '{print $2}'`
+  if [[ `dpkg -l $PACKAGE ` ]]; then
+     UNINSTALL_LIST=$UNINSTALL_LIST" "$PACKAGE 
+  fi
+done
+
+if [ -z ${UNINSTALL_LIST// /} ]; then 
+   echo ""
+   echo "Nothing to remove!"
+   echo ""
+   echo "Press Enter to exit"
+   read x
+   exit 0
+fi
+
+echo "Purge list: $UNINSTALL_LIST"
+
+sudo apt-get -y purge $UNINSTALL_LIST
+sudo apt-get -y autoremove
+
+echo "Press Enter to exit"
+read x
+
+exit 0
