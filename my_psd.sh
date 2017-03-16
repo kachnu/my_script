8056683dec0f54c4deac036fef04b62368240490
kachnu <ya.kachnu@yandex.ua>
2017-03-16 14:49:40 +0200

    Чтв_Мар_16_14:49:40_EET_2017

--- my_psd.sh
+++ my_psd.sh
@@ -22,11 +22,13 @@ case $LANG in
   uk*|ru*|be*) #UA RU BE locales
                MAIN_LABEL="Ускорение браузера Profile-sync-daemon"
                MAIN_TEXT="Выберите действие:"
-               MENU1="Старт/cтоп Profile-sync-daemon"
-               MENU2="Автозапуск Profile-sync-daemon"
-               MENU3="Состояние"
-               MENU4="Редактировать файл настроек"
-               MENU5="Очистить резервные копии профилей"
+               SS_PSD="Старт/cтоп Profile-sync-daemon"
+               AUTO_PSD="Автозапуск Profile-sync-daemon"
+               OVERLAYFS_PSD="Использование overlayfs"
+               STATE_PSD="Состояние"
+               EDIT_CONF_PSD="Редактировать файл настроек"
+               CLEAR_PSD="Очистить резервные копии профилей"
+               OVERLAYFS_ALARM="Ваше ядро не поддерживает режим overlayfs"
                MENUh="Справка"
                HELP="
 Profile-sync-daemon (psd) — небольшой псевдо-демон, предназначенный для переноса профилей браузеров в tmpfs (ОЗУ) и синхронизации с постоянным хранилищем (HDD/SSD) используя rsync.
@@ -38,7 +40,7 @@ Profile-sync-daemon (psd) — небольшой псевдо-демон, пре
 Профили, кэш, и прочие пользовательские данные браузера переносятся с помощью psd в tmpfs (ОЗУ), соответственно операции ввода/вывода браузера перенаправляются в оперативную память.
 Таким образом, уменьшается износ жесткого диска, повышается отказоустойчивость и скорость работы браузера.
 * Опционально включите использование overlayfs для улучшения скорости синхронизации и уменьшить количество необходимой памяти. 
-Для этого используйте переменную USE_OVERLAYFS. 
+Для этого используйте переменную USE_OVERLAYFS (или пункт скрипта). 
 Пользователю понадобятся sudo права доступа к /usr/bin/psd-overlay-helper для использования этой опции, а также ядро должно поддерживать overlayfs версии 22 или новее. 
 Смотрите FAQ на https://wiki.archlinux.org/index.php/Profile-sync-daemon.
 * Опционально укажите названия веб-браузеров, профили которых необходимо перенести в ОЗУ, посредством массива BROWSERS. 
@@ -51,11 +53,13 @@ Profile-sync-daemon (psd) — небольшой псевдо-демон, пре
             *) #all locales
                MAIN_LABEL="Browser Acceleration Profile-sync-daemon"
                MAIN_TEXT="Select an action:"
-               MENU1="Start/Stop Profile-sync-daemon"
-               MENU2="Autostart Profile-sync-daemon"
-               MENU3="Status"
-               MENU4="Edit the configuration file"
-               MENU5="Clear backup profiles"
+               SS_PSD="Start/Stop Profile-sync-daemon"
+               AUTO_PSD="Autostart Profile-sync-daemon"
+               OVERLAYFS_PSD="Use overlayfs"
+               STATE_PSD="Status"
+               EDIT_CONF_PSD="Edit the configuration file"
+               CLEAR_PSD="Clear backup profiles"
+               OVERLAYFS_ALARM="Your kernel does not support mode overlayfs"
                MENUh="Help"
                HELP="
 Profile-sync-daemonAUR is a tiny pseudo-daemon designed to manage browser profile(s) in tmpfs and to periodically sync back to the physical disc (HDD/SSD).
@@ -84,6 +88,12 @@ if [ -f $HOME/.config/systemd/user/default.target.wants/psd.service ]
  then STATE_AUTO="ON"
  else STATE_AUTO="OFF"
 fi
+
+if [[ `psd p | grep "Overlayfs" | grep "inactive"` ]]
+ then STATE_OVERLAYFS="OFF"
+ else STATE_OVERLAYFS="ON"
+fi
+
 }
 ##########################
 MainForm ()
@@ -92,11 +102,12 @@ CheckState
 ANSWER=$($DIALOG  --cancel-button "Exit" --title "$MAIN_LABEL" --menu \
     "$MAIN_TEXT" 15 60\
     7\
-        1 "$MENU1 - $STATE_RUN"\
-        2 "$MENU2 - $STATE_AUTO"\
-        3 "$MENU3"\
-        4 "$MENU4"\
-        5 "$MENU5"\
+        1 "$SS_PSD - $STATE_RUN"\
+        2 "$AUTO_PSD - $STATE_AUTO"\
+        3 "$OVERLAYFS_PSD - $STATE_OVERLAYFS"\
+        4 "$STATE_PSD"\
+        5 "$EDIT_CONF_PSD"\
+        6 "$CLEAR_PSD"\
         h "$MENUh" 3>&1 1>&2 2>&3)
 if [ $? == 0 ]
 then
@@ -113,21 +124,45 @@ then
         ;;
     2)  clear
         if [ "$STATE_AUTO" = "ON" ]
-         then systemctl --user disable	psd
+         then systemctl --user disable psd
          else systemctl --user enable psd
         fi 
         MainForm
         ;;
     3)  clear
+        if [ "$STATE_OVERLAYFS" != "ON" ]
+         then echo "OVERLAYFS on"
+              MY_KERN=`uname -r | awk -F. '{print $1"."$2}'`
+              NEED_KERN="3.18"
+              if [ $(echo "$MY_KERN >= $NEED_KERN" | bc) -eq 1 ]; then
+                 echo 'USE_OVERLAYFS="yes"' >> $HOME/.config/psd/psd.conf
+                 echo -e "$USER ALL=(ALL) NOPASSWD: /usr/bin/psd-overlay-helper" | sudo tee -a /etc/sudoers
+                 systemctl --user restart psd
+              else
+                 echo ""
+                 echo "$OVERLAYFS_ALARM"
+                 echo "Push Enter to Main menu"
+                 read x
+              fi
+         else echo "OVERLAYFS off"
+              sed -i '/^USE_OVERLAYFS=/d' $HOME/.config/psd/psd.conf
+              MY_USER=$USER
+              sudo sed -i "/${MY_USER}.*helper/d" /etc/sudoers
+              systemctl --user restart psd
+        fi
+        sleep 1
+        MainForm
+        ;;
+    4)  clear
         psd p
         echo "Push Enter to Main menu"
         read x
         MainForm
         ;;
-    4) $TEXT_EDITOR $HOME/.config/psd/psd.conf
+    5) $TEXT_EDITOR $HOME/.config/psd/psd.conf
         MainForm
         ;;
-    5)  clear
+    6)  clear
         psd c
         echo "Push Enter to Main menu"
         read x
