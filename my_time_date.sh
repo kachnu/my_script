e315c6e27a5fadf57180c7e4bc536b5b28ebb3b7
kachnu <ya.kachnu@yandex.ua>
2017-04-03 09:44:00 +0300

    Пнд_Апр__3_09:44:00_EEST_2017

--- my_time_date.sh
+++ my_time_date.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+# Set time and date
+# author: kachnu
+# email: ya.kachnu@yandex.ua
 
-if [ $(id -u) -ne 0 ]; then
-  echo "Restart $0 with root"
-  gksudo $0 
-  exit 0
-fi
+if ! [ `which yad` ]; then echo "Need yad"; exit 1; fi
+if ! [ `which timedatectl` ]; then echo "Need timedatectl"; exit 1; fi
+#if [ $(id -u) -ne 0 ]; then echo "Start $0 with root"; exit 1; fi
 
 Sync=$(timedatectl status | grep "Network" | awk '{print $4}')
 TZ=$(timedatectl status | grep "Time zone" | awk '{print $3}')
+TZ_list=$(timedatectl list-timezones | tr '\n' !)
 Hour=$(date +%H)
 Minute=$(date +%M)
 Second=$(date +%S)
 Date=$(date +%d.%m.%Y)
 
-TZ_list=$(timedatectl list-timezones | tr '\n' !)
-
-yad --title="Time and date" --form --separator="," \
+yad --window-icon=time-admin --title="Time and date" \
+--form --separator="," \
 --field="Sync time::CB" "$Sync!yes!no" \
 --field="Time zone::CB" "$TZ!$TZ_list" \
 --field="Hour::NUM" $Hour!0..23!1!0 \
@@ -33,27 +34,16 @@ yad --title="Time and date" --form --separator="," \
          Sync="true"
     else Sync="false"
     fi
-    if [ $Hour -lt 10 ]; then Hour=0$Hour;fi
-    if [ $Minute -lt 10 ]; then Minute=0$Minute;fi
-    if [ $Second -lt 10 ]; then Second=0$Second;fi
+    if [ $Hour -lt 10 ]; then Hour=0$Hour; fi
+    if [ $Minute -lt 10 ]; then Minute=0$Minute; fi
+    if [ $Second -lt 10 ]; then Second=0$Second; fi
     Date=`echo $Date| awk -F'.' '{print $3"-"$2"-"$1}'`
     Set_time=$Date" "$Hour":"$Minute":"$Second
 
-    echo $Sync $TZ $Hour $Minute $Second $Date
-    echo $Set_time
-    
     sudo timedatectl set-ntp false 
     sudo timedatectl set-timezone "$TZ"
     sudo timedatectl set-time "$Set_time"
     sudo timedatectl set-ntp "$Sync"
-
-#sudo /bin/bash - << usercodeblock
-#timedatectl set-ntp false 
-#timedatectl set-timezone "$TZ"
-#timedatectl set-time "$Set_time"
-#timedatectl set-ntp "$Sync"
-#usercodeblock
-    
 done
 
 exit 0
