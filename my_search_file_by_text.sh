dc7f192e3868e34f0c4b5f5da6b0db8f2c23b7f8
r2d2 <ya.kachnu@yandex.ua>
2017-04-21 00:25:09 +0300

    Птн_Апр_21_00:25:09_EEST_2017

--- my_search_file_by_text.sh
+++ my_search_file_by_text.sh
@@ -35,9 +35,8 @@ case $LANG in
 	формат результата - <адрес файла>:<номер строки в файле>:<содержание строки>
 
 ПРИМЕРЫ ИСПОЛЬЗОВАНИЯ СКРИПТА
-	$0 \"КОРОВА МИЛКА\" /home/user - выведет список файлов в /home/user содержащих фразу КОРОВА МИЛКА. 
+	$0 \"КОРОВА МИЛКА\" /home/user - выведет список файлов в /home/user содержащих фразу КОРОВА МИЛКА.
 Если фраза содержит пробелы, необходимо брать её в двойные ковычки \" \"
-	$0 --gui /home/user - откроет диалоговое окно, в котором по умолчанию будет выбрана папка поиска /home/user
 	$0 --gui - откроет диалоговое окно поиска
 	$0 -h - выводит справку"
                ;;
@@ -66,9 +65,8 @@ Results of the script
 	The result format - <address file>:<the line number in the file>:<line content>
 
 EXAMPLES OF SCRIPT
-	$0 \"Milka COW\" /home/user - will list the files in home/user containing the phrase Milka COW. 
+	$0 \"Milka COW\" /home/user - will list the files in home/user containing the phrase Milka COW.
 If the phrase contains spaces , you must take it in double quotes \" \"
-	$0 --gui /home/user - Opens a dialog box in which is selected by default Search Folder /home/user
 	$0 --gui - Open the search dialog
 	$0 -h - displays help"
                ;;
@@ -82,46 +80,58 @@ echo "$HELP"
 ###############################################
 GuiForm ()
 {
-if [ ! -x "`which "$DIALOG"`" ]
- then eсho "Install $DIALOG!"
-fi
-DIRECTORY="$1"
-ANSWER=$($DIALOG --width=400 --height=300 --list --cancel-label="Exit" --title="$MAIN_LABEL" \
-      --text="$MAIN_TEXT $DIRECTORY" \
-      --column="" --column="" \
-        1 "$MENU1"\
-        2 "$MENU2"\
-        3 "$MENU3" )
-if [ $? == 0 ]
-then
-  case $ANSWER in
-    1*) SEARCH_WORD=`$DIALOG --entry --cancel-label="Back" --title="Seach" --text="$SEARCH_TEXT" --entry-text="$SEARCH_WORD" `
-       if [ $? != 0 ]
-       then GuiForm "$DIRECTORY"
-       fi
-       if [[ $DIRECTORY == '' ]]
-       then $DIALOG --info --title="$ATTENTION" --text="$ERROR_DIR"
-            GuiForm "$DIRECTORY"
-       fi
-       if [[ $SEARCH_WORD == '' ]]
-       then $DIALOG --info --title="$ATTENTION" --text="$ERROR_WORD"
-            GuiForm "$DIRECTORY"
-       fi
-       grep -EHnr "$SEARCH_WORD" "$DIRECTORY" | $DIALOG --text-info --cancel-label="Back" --title="LIST" \
- --width=400 --height=300
-       GuiForm "$DIRECTORY"
-       ;;
-    2*) DIRECTORY=`$DIALOG --file-selection --directory --title="$DIRECTORY_TEXT" --filename="$DIRECTORY"`
-       GuiForm "$DIRECTORY"
-       ;;
-    3*) Help | $DIALOG --text-info --cancel-label="Back" --title="Help" --width=400 --height=300
-       GuiForm "$DIRECTORY"
-       ;;              
-    *) echo oops!-$ANSWER
-       exit 1
-       ;;
- esac
-else echo Exit ; exit 0
+if [ $DIALOG  == "zenity" ]; then
+    DIRECTORY="$1"
+    ANSWER=$($DIALOG --width=400 --height=300 --list --cancel-label="Exit" --title="$MAIN_LABEL" \
+          --text="$MAIN_TEXT $DIRECTORY" \
+          --column="" --column="" \
+            1 "$MENU1"\
+            2 "$MENU2"\
+            3 "$MENU3" )
+    if [ $? == 0 ]
+    then
+      case $ANSWER in
+        1*) SEARCH_WORD=`$DIALOG --entry --cancel-label="Back" --title="Seach" --text="$SEARCH_TEXT" --entry-text="$SEARCH_WORD" `
+           if [ $? != 0 ]
+           then GuiForm "$DIRECTORY"
+           fi
+           if [[ $DIRECTORY == '' ]]
+           then $DIALOG --info --title="$ATTENTION" --text="$ERROR_DIR"
+                GuiForm "$DIRECTORY"
+           fi
+           if [[ $SEARCH_WORD == '' ]]
+           then $DIALOG --info --title="$ATTENTION" --text="$ERROR_WORD"
+                GuiForm "$DIRECTORY"
+           fi
+           grep -EHnr "$SEARCH_WORD" "$DIRECTORY" | $DIALOG --text-info --cancel-label="Back" --title="LIST" \
+     --width=400 --height=300
+           GuiForm "$DIRECTORY"
+           ;;
+        2*) DIRECTORY=`$DIALOG --file-selection --directory --title="$DIRECTORY_TEXT" --filename="$DIRECTORY"`
+           GuiForm "$DIRECTORY"
+           ;;
+        3*) Help | $DIALOG --text-info --cancel-label="Back" --title="Help" --width=400 --height=300
+           GuiForm "$DIRECTORY"
+           ;;
+        *) echo oops!-$ANSWER
+           exit 1
+           ;;
+     esac
+    else echo Exit ; exit 0
+    fi
+else
+    SEARCH_WORD=`echo $* | awk -F',' '{print $1}'`
+    DIRECTORY=`echo $* | awk -F',' '{print $2}'`
+    INFO=`yad --width=300 --window-icon=gtk-find --title="$MAIN_LABEL" \
+    --form --separator="," \
+    --field="Text:" "$SEARCH_WORD" \
+    --field="Folder::DIR" "$DIRECTORY"`
+    if [ $? != 0 ]; then exit; fi
+    SEARCH_WORD=`echo $INFO | awk -F',' '{print $1}'`
+    DIRECTORY=`echo $INFO | awk -F',' '{print $2}'`
+    grep -EHnr --color "$SEARCH_WORD" "$DIRECTORY" | $DIALOG --text-info --width=600 --height=300 \
+    --window-icon=gtk-find --title="$MAIN_LABEL"
+    GuiForm "$SEARCH_WORD"",""$DIRECTORY"
 fi
 }
 ###############################################
