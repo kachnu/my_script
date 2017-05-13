16b830167427990e45a903e0a53e687ccebba03b
kachnu <ya.kachnu@yandex.ua>
2017-05-13 09:05:09 +0300

    Суб_Май_13_09:05:09_EEST_2017

---
+++ CRUNCH/my_lockkey_stat.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+# xfce 4.12
+# Display key NumLock, CapsLock, ScrollLock
+# author: kachnu
+# email:  ya.kachnu@yandex.ua
+
+OPT=$1
+
+SCRIPT_WAY=`readlink -e "$0"`
+
+KEY_INFO=`xset -q | grep -m1 "00:" | sed "s/ //g"`
+
+DIALOG=yad
+if ! [[ `which $DIALOG` ]]
+   then DIALOG=zenity
+fi
+
+MakePlugin ()
+{
+# find panel
+PANEL=`xfconf-query -c xfce4-panel -p /panels -v | awk '{print $1}' | grep [0-9] | sed 's/^/panel-/g'`
+
+# select panel
+PANEL=`echo "$PANEL" | sed "s/^ //g" | sed "s/ /\\\n/g" | $DIALOG --list --title="Add lock-key plugin" \
+                --text="select panel" --column="" --separator="\n"`
+
+if [ $? != 0 ]; then
+   exit 0
+fi
+
+#find max id
+max_id=0
+for id in `xfconf-query -c xfce4-panel -p /plugins -l -v | awk '{print $1}'| awk -F/ '{print $3}'| awk -F- '{print $2}'`; do
+  if [ "$id" -gt "$max_id" ]; then
+     max_id=$id
+  fi
+done
+
+# new id for plugin
+let new_id=$max_id+1
+
+#create file-plugin
+echo -e "Command=$SCRIPT_WAY -S
+UseLabel=0
+Text=(genmon)
+UpdatePeriod=1000
+Font=(default)" > ~/.config/xfce4/panel/genmon-$new_id.rc
+
+# add plugin to xfconf
+xfconf-query -c xfce4-panel -p /plugins/plugin-$new_id -t string -s "genmon" --create
+
+# add new id to plugin-ids
+new_id_list=""
+
+if [[ `xfconf-query -c xfce4-panel -p /panels/$PANEL/plugin-ids` ]]; then
+      for id in `xfconf-query -c xfce4-panel -p /panels/$PANEL/plugin-ids| grep -v "Value is an\|^$" | grep -v :`; do
+          new_id_list=$new_id_list" -t int -s "$id
+      done
+      new_id_list=$new_id_list" -t int -s "$new_id
+      echo $new_id_list
+      xfconf-query -c xfce4-panel -p /panels/$PANEL/plugin-ids -rR
+      xfconf-query -c xfce4-panel -p /panels/$PANEL/plugin-ids $new_id_list --create
+else  xfconf-query -c xfce4-panel -p /panels/$PANEL/plugin-ids --force-array -t int -s $new_id --create
+fi
+
+# restart panel
+xfce4-panel -r
+}
+
+case $OPT in
+    -s) NL="Num"
+        CL="Caps"
+        SL="Scr"
+        SPASE=" ";;
+    -S) NL="NUM"
+        CL="CAPS"
+        SL="SCR"
+        SPASE=" ";;
+    -p) MakePlugin;;
+    -h|--help) clear
+echo -e "Script `basename $SCRIPT_WAY` designed to display key NumLock, CapsLock, ScrollLock
+
+Options
+    -s 		to display short value, like Num, Caps, Scr
+    -S 		to display short value, like NUM, CAPS, SCR
+    -p 		make plugin in xfce4-panel 
+    -h, --help 	to help ";;
+    *)  NL="NumLock"
+        CL="CapsLock"
+        SL="ScrollLock"
+        SPASE=" 	 ";;
+esac
+
+if [[ `echo "$KEY_INFO"| grep "CapsLock:off"` ]]; then CL=`echo $CL | sed "s/[a-zA-Z]/  /g"`; fi
+if [[ `echo "$KEY_INFO"| grep "NumLock:off"` ]]; then NL=`echo $NL | sed "s/[a-zA-Z]/  /g"`; fi
+if [[ `echo "$KEY_INFO"| grep "ScrollLock:off"` ]]; then SL=`echo $SL | sed "s/[a-zA-Z]/  /g"`; fi
+
+echo -e "$NL$SPASE$CL$SPASE$SL"
+
+exit 0
---
+++ CRUNCH/my_net_stat.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+OLD_INFO=`cat /dev/shm/netstatold || netstat -i -e`
+OLD_TIME=`cat /dev/shm/uptimeold | awk '{print $1}' || cat /proc/uptime | awk '{print $1}'`
+
+netstat -i -e > /dev/shm/netstatold
+cat /proc/uptime > /dev/shm/uptimeold
+
+
+if [[ `echo -e "$OLD_INFO" | grep UP | grep RUNNING | grep -v lo | awk -F: '{print $1}'` ]]; then
+   IP_WLAN=`wget -O - -q icanhazip.com`
+   echo -e "IP WAN: 		$IP_WLAN
+"
+fi
+
+
+
+#for GW in `ip r|grep default|awk '{print $3}'`; do
+    #echo -e "GW:			$GW"
+#done
+
+#NUM=0
+#for DNS in `cat /etc/resolv.conf | grep -v \# | grep nameserver | awk '{print $2}'`; do
+    #NUM=$(($NUM+1))
+    #echo -e "DNS $NUM: 		$DNS"
+#done
+##############################################################
+#OLD_INFO=`netstat -i -e`
+#OLD_TIME=`cat /proc/uptime | awk '{print $1}'`
+#sleep 1
+NET_DEV=`echo -e "$OLD_INFO" | grep UP | grep RUNNING | grep -v lo | awk -F: '{print $1}'`
+for DEV in $NET_DEV; do
+     IP_DEV=`echo -e "$OLD_INFO" | grep -A8 $DEV | grep "inet " | awk '{print $2}'`
+     GW=`ip r|grep default| grep $DEV| awk '{print $3}'`
+     # Speed test
+     OLD_BIT_RX=`echo -e "$OLD_INFO" | grep -A8 $DEV | grep "RX packets" | awk '{print $5}'`
+     OLD_BIT_TX=`echo -e "$OLD_INFO" | grep -A8 $DEV | grep "TX packets" | awk '{print $5}'`     
+     NEW_INFO=`netstat -i -e`
+     NEW_TIME=`cat /proc/uptime | awk '{print $1}'`
+     NEW_BIT_RX=`echo -e "$NEW_INFO" | grep -A8 $DEV | grep "RX packets" | awk '{print $5}'`
+     NEW_BIT_TX=`echo -e "$NEW_INFO" | grep -A8 $DEV | grep "TX packets" | awk '{print $5}'`
+     SPEED_RX=$(echo "scale=2; ($NEW_BIT_RX - $OLD_BIT_RX)/($NEW_TIME-$OLD_TIME)/1024" | bc)
+     SPEED_TX=$(echo "scale=2; ($NEW_BIT_TX - $OLD_BIT_TX)/($NEW_TIME-$OLD_TIME)/1024" | bc)
+     # Total rx/tx
+     RX_DEV=`echo -e "$NEW_INFO" | grep -A8 $DEV | grep "RX packets" | awk -F"(" '{print $2}' | awk -F")" '{print $1}'`
+     TX_DEV=`echo -e "$NEW_INFO" | grep -A8 $DEV | grep "TX packets" | awk -F"(" '{print $2}' | awk -F")" '{print $1}'`
+     # Print net info
+     echo -e "IP $DEV: 		$IP_DEV
+GW: 			$GW
+RX: 			$SPEED_RX kBps/ $RX_DEV
+TX: 			$SPEED_TX kBps/ $TX_DEV
+"
+done
+
+
+#################VERSION2##############################################
+####fix_time_1s
+#OLD_INFO=`cat /proc/net/dev`
+#sleep 1
+#NEW_INFO=`cat /proc/net/dev`
+#NET_DEV=`echo -e "$OLD_INFO" | grep : | grep -v lo | sed "s/ //g" | grep -v ":00"| awk -F: '{print $1}'`
+#IP_INFO=`ip a`
+#for DEV in $NET_DEV; do
+     #IP_DEV=`echo -e "$IP_INFO" | grep -A4 $DEV | grep "inet " | awk '{print $2}'`
+     #GW=`ip r|grep default| grep $DEV| awk '{print $3}'`
+     ## Speed test
+     #OLD_BIT_RX=`echo -e "$OLD_INFO" | grep $DEV | awk '{print $2}'`
+     #OLD_BIT_TX=`echo -e "$OLD_INFO" | grep $DEV | awk '{print $10}'`     
+     #NEW_BIT_RX=`echo -e "$NEW_INFO" | grep $DEV | awk '{print $2}'`
+     #NEW_BIT_TX=`echo -e "$NEW_INFO" | grep $DEV | awk '{print $10}'`
+     #SPEED_RX=$(echo "scale=2; ($NEW_BIT_RX - $OLD_BIT_RX)/1024" | bc)
+     #SPEED_TX=$(echo "scale=2; ($NEW_BIT_TX - $OLD_BIT_TX)/1024" | bc)
+     ## Total rx/tx
+     #RX_DEV=$(echo "scale=1; $NEW_BIT_RX/1024/1024" | bc)
+     #TX_DEV=$(echo "scale=1; $NEW_BIT_TX/1024/1024" | bc)
+     ## Print net info
+     #echo -e "IP $DEV: 		$IP_DEV
+#GW: 			$GW
+#RX: 			$SPEED_RX kBps/ $RX_DEV MiB
+#TX: 			$SPEED_TX kBps/ $TX_DEV MiB
+#"
+#done
+##################VERSION3#############################################
+####time
+
+#OLD_INFO=`cat /dev/shm/netstatold || netstat -i -e`
+#OLD_TIME=`cat /dev/shm/uptimeold | awk '{print $1}' || cat /proc/uptime | awk '{print $1}'`
+
+#cat /proc/net/dev > /dev/shm/netstatold
+#cat /proc/uptime > /dev/shm/uptimeold
+
+#NET_DEV=`echo -e "$OLD_INFO" | grep : | grep -v lo | sed "s/ //g" | grep -v ":00"| awk -F: '{print $1}'`
+#IP_INFO=`ip a`
+#for DEV in $NET_DEV; do
+     #IP_DEV=`echo -e "$IP_INFO" | grep -A4 $DEV | grep "inet " | awk '{print $2}'`
+     #GW=`ip r|grep default| grep $DEV| awk '{print $3}'`
+     ## Speed test
+     #OLD_BIT_RX=`echo -e "$OLD_INFO" | grep $DEV | awk '{print $2}'`
+     #OLD_BIT_TX=`echo -e "$OLD_INFO" | grep $DEV | awk '{print $10}'`     
+     #NEW_INFO=`cat /proc/net/dev`
+     #NEW_TIME=`cat /proc/uptime | awk '{print $1}'`
+     #NEW_BIT_RX=`echo -e "$NEW_INFO" | grep $DEV | awk '{print $2}'`
+     #NEW_BIT_TX=`echo -e "$NEW_INFO" | grep $DEV | awk '{print $10}'`
+     #SPEED_RX=$(echo "scale=2; ($NEW_BIT_RX - $OLD_BIT_RX)/($NEW_TIME-$OLD_TIME)/1024" | bc)
+     #SPEED_TX=$(echo "scale=2; ($NEW_BIT_TX - $OLD_BIT_TX)/($NEW_TIME-$OLD_TIME)/1024" | bc)
+     ## Total rx/tx
+     #RX_DEV=$(echo "scale=1; $NEW_BIT_RX/1024/1024" | bc)
+     #TX_DEV=$(echo "scale=1; $NEW_BIT_TX/1024/1024" | bc)
+     ## Print net info
+     #echo -e "IP $DEV: 		$IP_DEV
+#GW: 			$GW
+#RX: 			$SPEED_RX kBps/ $RX_DEV MiB
+#TX: 			$SPEED_TX kBps/ $TX_DEV MiB
+#"
+#done
+
+
+
+exit 0
