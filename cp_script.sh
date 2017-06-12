4ba423962318a69be502b1d724782d5e46599dbe
kachnu <ya.kachnu@yandex.ua>
2017-06-12 10:54:38 +0300

    Пнд_Июн_12_10:54:38_EEST_2017

---
+++ cp_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SOURSE_DIR="."
+REMOTE_DIR="/usr/local/bin"
+for SOUR in `ls $SOURSE_DIR`; do
+    for REM in `ls $REMOTE_DIR`; do
+        if [ "$REM" = "$SOUR" ]; then
+           sudo cp "$SOURSE_DIR/$SOUR" "$REMOTE_DIR/$REM" && sudo chmod +x "$REMOTE_DIR/$REM"
+           echo "new $REMOTE_DIR/$REM"
+        fi
+    done
+done
+
+echo ""
+echo "Push Enter"
+read x
+
+exit 0
